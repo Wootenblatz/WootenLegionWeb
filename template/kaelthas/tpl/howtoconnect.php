0653b626638e7a57a9c84a22fc8678bdbbdebdb9
Wootenblatz <zkarpinski@gmail.com>
2021-09-05 20:20:52 -0500

    Updated styles and instructions

--- template/kaelthas/tpl/howtoconnect.php
+++ template/kaelthas/tpl/howtoconnect.php
@@ -6,16 +6,15 @@
  * Date: 11/26/2018 - 8:36 PM
  */
 ?>
-<div style="text-align: left;padding: 10px;line-height: 1.5;">
-    <p>1. First of all, you must create an account. The account is used to log into both the game and our website. Click
-        here to open the registration page.</p>
-    <p>2. Install World of Warcraft. You can download it (legally) from here: Windows or Mac. Make sure to upgrade to
-        our current supported patch, which is 7.3.5 (build 26124-26972). Patch mirrors can be found here. </p>
-    <p>3. Open up the "World of Warcraft" directory. The default directory is "C:\Program Files\World of Warcraft". When
-        you've found it, open up the directory called "data", then go into the directory called either enUS or enGB,
-        depending on your client language.</p>
-    <p>5. Erase all text and change it to:</p>
-    <p style="text-align: center;font-weight: bold;color:#F1A40F">set
-        portal "<?php echo get_config('realmlist'); ?>"</p>
-    <p>You may now start playing! If you need any help, do not hesitate to create a support ticket.</p>
+<div class="row">
+    <div class='col-md-12'>
+        <h1>Connection Instructions</h1>
+        <ol>
+            <li>Open up the directory you put the Legion WoW client files in</li>
+            <li>If you do not see a directory called <code>WTF</code>, run WoW by double clicking <code>WoW.exe</code>.  Exit the game.</li>
+            <li>Use your favorite text editor to open <code>WTF/Config.wtf</code></li>
+            <li>Update the first line so that it says <code>SET portal "wootenmane.wootenblatz.com"</code></li>
+            <li>Save the file and run the game</li>
+        </ol>
+    </div>
 </div>
--- template/kaelthas/tpl/posts.php
+++ template/kaelthas/tpl/posts.php
@@ -1,6 +1,63 @@
 <div class="box1">
     <h1>Welcome to Wootenmane!</h1>
-    <p>
-        You can create your game account under the Registration button on the top.  After that you should visit the page linked in "How to connect" in the header above.
-    </p>
+
+<h3><strong>Step 1 – WoW 7.3.5 (for Legion)</strong></h3>
+<p>Legion is available from this page <a href="https://wowmortal.com/download.php">https://wowmortal.com/download.php</a> as a &quot;Magnet link&quot; if you click &quot;7.3.5 Full Client Torrent(Magnet)&quot;.  You will need to install a bit torrent client that can open this kind of link before trying to open it.  You can also google where to download Legion if this method does not work for you.</p>
+<p>If you do not have a bit torrent client, I like Transmission: <a href="https://transmissionbt.com/">https://transmissionbt.com/</a></p>
+<p>You can also download the client directly from <a href='/LegionClient.zip'>here</a></p>
+<hr width='100%' size='1' />
+<h3><strong>Step 2 – Visit the signup page to create your account(s)</strong></h3>
+<p>In your favorite browser, open <a href="http://wootenmane.wootenblatz.com">http://wootenmane.wootenblatz.com</a>.  This page is password protected! However you're currently viewing the site so you already know that!  Use the login credentials below to enter:</p>
+<p><em>Username:</em> <code>wootenblatz</code><br>
+<em>Password:</em> <code>tootsAlot</code></p>
+<br>
+<h3>To register, follow these steps</h3>
+<ol>
+<li>Click Register in the navigation at the top of the page</li>
+<li>Enter something that looks like an email address.<br><div class="hint">
+You do not have to use your real email address (the system does not send emails)
+</div></li>
+<li>Enter a number from 1 to 5, for how many accounts you would like to have tied to your &quot;Battle.net&quot; account.</li>
+<li>Enter a password and confirm the password</li>
+</ol>
+<hr width='100%' size='1' />
+<h3><strong>Step 3 - Get some Addons (Optional)</strong></h3>
+<p>Legion addons are available from these sites.  You may need to search for older versions or filter for addons that work with 7.3.5.</p>
+<ul>
+<li><a href="https://firestorm-servers.com/en/addons/index">https://firestorm-servers.com/en/addons/index</a></li>
+<li><a href="https://addonswow.com/">https://addonswow.com/</a></li>
+<li>I recreated a 7.3.5 compatible version of GSE from github releases<br>
+    <a href="https://cdn.discordapp.com/attachments/840931328870711310/881197663587041341/GSE-7.3.5.zip">Download GSE-7.3.5.zip from this Server's Discord</a></li>
+</ul>
+<hr width='100%' size='1' />
+<h3><strong>Step 4 - Start the game and make some characters!</strong></h3>
+<p>Notes on character creation:
+Death Knights, Demon Hunters and Allied races are available if you have at least one level 1 character on your account.  If you want to play as any of these, create a regular character first, log out and then you can create a new toon with the heroic classes and allied races.</p>
+<p>This server is running a very complete repack that supports the following features:</p>
+<ul>
+<li>Dynamic XP per character</li>
+<li>Very complete quest system with few bugs</li>
+<li>Optional skips for Death Knights, Demon Hunters, Pandarens, Goblins and Worgen.  Check your inventory for the scroll that lets you skip these areas.</li>
+<li>A battle.net like shop that lets you convert game gold into battle coins to buy mounts, heirlooms, boosts and more.</li>
+<li>Mythic Plus Dungeons</li>
+<li>Allied races available at level 1</li>
+<li>Full cross faction interactions are enabled! Horde and Alliance can<ul>
+<li>Chat with each other, including using emotes</li>
+<li>Trade, buy/sell on the AH and send mail with each other</li>
+<li>Join the same party, raid and guild</li>
+<li>Add other side to friends list and see players with /who</li>
+</ul>
+</li>
+<li>There is an auction house bot that will buy and sell items automatically</li>
+<li>Every time you use a gathering node, you will receive 5x the materials you would have gotten normally.</li>
+<li>Professions level up at a 5x rate with higher combines for yellow and green receipes.</li>
+<li>You can learn all professions on a single character</li>
+<li>Guild charters require 2 signatures and the fees are drastically lower</li>
+<li>All maps explored at character creation</li>
+<li>All flight paths known at character creation</li>
+<li>Resetting your talents is free</li>
+<li>Increased reputation gains, including for low level kills and quests</li>
+<li>Max weapon skill upon level up</li>
+</ul>
+
 </div>
